62dcfe15b74ebd713a8cfc4127b3591c4b3c25e2
anfishka <97487005+anfishka@users.noreply.github.com>
2024-02-01 17:29:11 +0200

    Add files via upload

---
+++ mock_data3.sql
@@ -0,0 +1,44 @@
+
+
+CREATE DATABASE mock_data3_db;
+USE mock_data3_db;
+
+CREATE TABLE MOCK_DATA(
+trader_id INT,
+first_name VARCHAR(50),
+last_name VARCHAR(50),
+age INT,
+email VARCHAR(50),
+country VARCHAR(50),
+postal_code VARCHAR(50) NULL,
+favorite_color VARCHAR(50),
+purchase_price  DECIMAL(15,2),
+sale_price DECIMAL(15,2),
+profit  DECIMAL(15,2)
+);
+
+SELECT *
+FROM 
+	[MOCK_DATA]
+WHERE 
+	country = 'China';
+
+SELECT *
+FROM 
+	[MOCK_DATA]
+WHERE 
+	postal_code = '9660' 
+OR
+	postal_code = '3078'
+OR 
+	postal_code = '6149' 
+OR 
+	postal_code = '2040';
+
+--Вывести все у чего прибыль больше 5%
+
+SELECT *
+FROM 
+	[MOCK_DATA]
+WHERE 
+	(((sale_price-purchase_price)/sale_price) * 100) > 5;
